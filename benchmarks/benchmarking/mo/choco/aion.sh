8bf01eb9247923790ef313544db12c10b3d9846b
Manuel Combarro Simon <manuel.combarrosimon@uni.lu>
2024-06-06 02:51:29 +0200

    Hot fix to use modules installed by easy build

--- benchmarks/benchmarking/mo/choco/aion.sh
+++ benchmarks/benchmarking/mo/choco/aion.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-module load compiler/GCCcore/10.2.0
+# See installation instructions for Choco.
+module use $HOME/.local/easybuild/${ULHPC_CLUSTER}/turbo/${RESIF_ARCH}/modules/all
+
 module load lang/Python/3.8.6-GCCcore-10.2.0
 module load lang/Java/21.0.2 # for Choco
