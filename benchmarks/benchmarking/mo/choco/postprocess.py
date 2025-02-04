95a456eed57cdaedd725818fda2c5f2bef70bd7e
Manuel Combarro Simon <manuel.combarrosimon@uni.lu>
2025-02-04 22:27:22 +0100

    Refactor of postprocess

--- benchmarks/benchmarking/mo/choco/postprocess.py
+++ benchmarks/benchmarking/mo/choco/postprocess.py
@@ -9,6 +9,138 @@ from pymoo.indicators.hv import Hypervolume
 import re
 
 
+def process_json_file(input_json_file_path, output_stats_filename):
+    with open(input_json_file_path, 'r') as file:
+        lines = file.readlines()
+
+    current_json_mo_solution_details = {}
+    exceptions = []
+    errors = []
+    statistics = {}
+
+    # We keep successive experiments in the JSON file, even when they fail.
+    # For the statistics, we are only interested by the latest experiment,
+    # starting with {"type": "lattice-land", "lattice-land": "start"}.
+    for line in lines:
+        if is_valid_json(line):
+            output = json.loads(line)
+            if output["type"] == "lattice-land":
+                if output["lattice-land"] == "start":
+                    statistics = {}
+                    exceptions = []
+                    errors = []
+                    current_json_mo_solution_details = {}
+            elif output["type"] == "statistics":
+                statistics.update(output["statistics"])
+            elif output["type"] == "solutions-details":
+                current_json_mo_solution_details.update(output["solutions-details"])
+            elif output["type"] == "error":
+                errors += line
+        elif "exception" in line.lower():
+            exceptions.append(line)
+
+    if errors != [] or exceptions != []:
+        print(f"Error in {sys.argv[2]}", file=sys.stderr)
+        for error in errors:
+            print(error, file=sys.stderr)
+        for exception in exceptions:
+            print(exception, file=sys.stderr)
+
+    statistics_fields = list(statistics.keys())
+    solution_details_fields = list(current_json_mo_solution_details.keys())
+    if "solver_messages" in solution_details_fields:
+        solution_details_fields.remove("solver_messages")
+
+    filtered_data = {}
+    front_metrics = {}
+    if statistics:
+        filtered_data = OrderedDict({field: statistics.get(field, None) for field in statistics_fields})
+
+    if not current_json_mo_solution_details:
+        instance_path = Path(sys.argv[2])
+        instance_file = instance_path.name
+        print(f"No experiment data for: {instance_file}. File: {sys.argv[2]}", file=sys.stderr)
+
+    reference_point = False
+    # if current_json_mo_solution_details:
+    if 'pareto_front' in current_json_mo_solution_details:
+        if "reference_point" in current_json_mo_solution_details:
+            reference_point = current_json_mo_solution_details.get("reference_point", None)
+            current_json_mo_solution_details.pop("reference_point", None)
+            hypervolume = calculate_hypervolume(np.array(current_json_mo_solution_details.get('pareto_front')),
+                                                np.array(reference_point))
+            front_metrics.update({"hypervolume": hypervolume})
+            if 'all_solutions' in current_json_mo_solution_details:
+                solutions = current_json_mo_solution_details.get('all_solutions')
+            else:
+                solutions = current_json_mo_solution_details.get('pareto_front')
+            front_metrics.update({"front_cardinality": len(current_json_mo_solution_details.get('pareto_front'))})
+            # calculate hypervolume evolution
+            if calculate_evolution and (calculate_evolution_for_gavanelli or filtered_data['front_generator'] !=
+                                        'ParetoGavanelliGlobalConstraint'):
+                hypervolume_evolution = [0.0] * len(solutions)
+                temp_front = []
+                for index, point in enumerate(solutions):
+                    temp_front.append(point)
+                    hypervolume_evolution[index] = float(calculate_hypervolume(np.array(temp_front),
+                                                                               np.array(reference_point)))
+                front_metrics.update({"hypervolume_evolution": hypervolume_evolution})
+            else:
+                front_metrics.update({"hypervolume_evolution": "Not available."})
+            front_metrics["all_solutions"] = solutions
+    front_metrics["solutions_in_time"] = "Not available."
+    if "solver_messages" in current_json_mo_solution_details:
+        if "choco" in statistics["solver"] and calculate_evolution and (
+                calculate_evolution_for_gavanelli or filtered_data['front_generator'] !=
+                'ParetoGavanelliGlobalConstraint'):
+            solutions_in_time = get_solutions_in_time_for_choco(current_json_mo_solution_details["solver_messages"],
+                                                                'all_solutions' in current_json_mo_solution_details,
+                                                                filtered_data['front_generator'] ==
+                                                                'ParetoGavanelliGlobalConstraint')
+            front_metrics["solutions_in_time"] = solutions_in_time
+        current_json_mo_solution_details.pop("solver_messages")
+    filtered_data.update({field: current_json_mo_solution_details.get(field, None) for field
+                          in solution_details_fields})
+    if reference_point:
+        filtered_data["reference_point"] = reference_point
+
+    filtered_data.update(front_metrics)
+    all_fields = list(filtered_data.keys())
+    # Move 'reference_point' to the last position
+    if "reference_point" in all_fields:
+        all_fields.remove("reference_point")
+    all_fields.append("reference_point")
+
+    if current_json_mo_solution_details:
+        double_check_gavanelli_front_strategy_stats_are_correc_for_choco(filtered_data,
+                                                                         output["solutions-details"]["solver_messages"])
+
+    output_file_exists = os.path.isfile(output_stats_filename)
+    existing_data = []
+    if output_file_exists:
+        with open(output_stats_filename, 'r', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            existing_data = [row for row in reader]
+            existing_headers = reader.fieldnames
+            headers = existing_headers + [field for field in all_fields if field not in existing_headers]
+            # Ensure 'reference_point' is last
+            if "reference_point" in headers:
+                headers.remove("reference_point")
+            headers.append("reference_point")
+    else:
+        headers = all_fields
+
+    with open(output_stats_filename, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=headers)
+        writer.writeheader()
+
+        if existing_data:
+            for row in existing_data:
+                writer.writerow(row)
+        writer.writerow(filtered_data)
+    print(f"Processed {sys.argv[2]}")
+
+
 def is_valid_json(json_str):
     try:
         json.loads(json_str)
@@ -80,7 +212,8 @@ def double_check_gavanelli_front_strategy_stats_are_correc_for_choco(processed_d
                 else:
                     instance_msg = processed_data.get('instance', 'Unknown instance')
                     raise ValueError(
-                        f"Error: Field 'Nodes' not found in the last solution details message for Gavanelli instance '{instance_msg}'."
+                        f"Error: Field 'Nodes' not found in the last solution details message for Gavanelli instance "
+                        f"'{instance_msg}'."
                     )
             else:
                 match = re.search(rf'{re.escape(value)}\s*:\s*([\d,]+(?:\.\d+)?)', last_solution_details_messages)
@@ -91,7 +224,8 @@ def double_check_gavanelli_front_strategy_stats_are_correc_for_choco(processed_d
                 else:
                     instance_msg = processed_data.get('instance', 'Unknown instance')
                     raise ValueError(
-                        f"Error: Field '{value}' not found in the last solution details message for Gavanelli instance '{instance_msg}'."
+                        f"Error: Field '{value}' not found in the last solution details message for Gavanelli instance "
+                        f"'{instance_msg}'."
                     )
         except ValueError as e:
             print(e)
@@ -100,123 +234,14 @@ def double_check_gavanelli_front_strategy_stats_are_correc_for_choco(processed_d
 
 if __name__ == "__main__":
     csv.field_size_limit(sys.maxsize)
-    calculate_evolution_for_gavanelli = False # if the evolution of the hypervolume is not needed, set this to False, as it could take a lot of time
+    calculate_evolution_for_gavanelli = False  # if the evolution of the hypervolume is not needed, set this to False,
+    # as it could take a lot of time
+    calculate_evolution = False  # turn off the computation of the hypervolume evolution, it could be very expensive
     output_dir = sys.argv[1]
     if output_dir[-1] == "/":
         output_dir = output_dir[:-1]
     input_file_path = Path(sys.argv[2])
     output_file_path = Path(output_dir)
     uid = os.path.basename(os.path.normpath(output_file_path))
-    # uid = input_file_path.stem
     sol_stats_filename = Path(output_dir + "/mo_" + uid + "_solutions_and_stats.csv")
-    # stats_filename = Path(output_dir + "/" + uid + "_stats.yml")
-
-    with open(input_file_path, 'r') as file:
-        lines = file.readlines()
-
-    json_objects = []
-    current_json_mo_solution_details = {}
-    unknowns = []
-    exceptions = []
-    errors = []
-    statistics = {}
-
-    # We keep successive experiments in the JSON file, even when they fail.
-    # For the statistics, we are only interested by the latest experiment,
-    # starting with {"type": "lattice-land", "lattice-land": "start"}.
-    for line in lines:
-        if is_valid_json(line):
-            output = json.loads(line)
-            if output["type"] == "lattice-land":
-                if output["lattice-land"] == "start":
-                    statistics = {}
-                    exceptions = []
-                    errors = []
-                    current_json_mo_solution_details = {}
-            elif output["type"] == "statistics":
-                statistics.update(output["statistics"])
-            elif output["type"] == "solutions-details":
-                current_json_mo_solution_details.update(output["solutions-details"])
-            elif output["type"] == "error":
-                errors += line
-        elif "exception" in line.lower():
-            exceptions.append(line)
-
-    if errors != [] or exceptions != []:
-        print(f"Error in {sys.argv[2]}", file=sys.stderr)
-        for error in errors:
-            print(error, file=sys.stderr)
-        for exception in exceptions:
-            print(exception, file=sys.stderr)
-
-    statistics_fields = list(statistics.keys())
-    solution_details_fields = list(current_json_mo_solution_details.keys())
-    if "solver_messages" in solution_details_fields:
-        solution_details_fields.remove("solver_messages")
-
-    filtered_data = {}
-    solutions_in_time = []
-    front_metrics = {}
-    if statistics:
-        filtered_data = OrderedDict({field: statistics.get(field, None) for field in statistics_fields})
-    if current_json_mo_solution_details:
-        if 'pareto_front' in current_json_mo_solution_details:
-            if "reference_point" in current_json_mo_solution_details:
-                reference_point = current_json_mo_solution_details.get("reference_point", None)
-                current_json_mo_solution_details.pop("reference_point")
-                hypervolume = calculate_hypervolume(np.array(current_json_mo_solution_details.get('pareto_front')), np.array(reference_point))
-                front_metrics.update({"hypervolume": hypervolume})
-                if 'all_solutions' in current_json_mo_solution_details:
-                    solutions = current_json_mo_solution_details.get('all_solutions')
-                else:
-                    solutions = current_json_mo_solution_details.get('pareto_front')
-                front_metrics.update({"front_cardinality": len(current_json_mo_solution_details.get('pareto_front'))})
-                # calculate hypervolume evolution
-                if calculate_evolution_for_gavanelli or filtered_data['front_generator'] != 'ParetoGavanelliGlobalConstraint':
-                    hypervolume_evolution = [0] * len(solutions)
-                    temp_front = []
-                    for index, point in enumerate(solutions):
-                        temp_front.append(point)
-                        hypervolume_evolution[index] = float(calculate_hypervolume(np.array(temp_front),
-                                                                             np.array(reference_point)))
-                    front_metrics.update({"hypervolume_evolution": hypervolume_evolution})
-                else:
-                    front_metrics.update({"hypervolume_evolution": "Not available."})
-                front_metrics["all_solutions"] = solutions
-        front_metrics["solutions_in_time"] = "Not available."
-        if "solver_messages" in current_json_mo_solution_details:
-            if "choco" in statistics["solver"] and (calculate_evolution_for_gavanelli or filtered_data['front_generator'] != 'ParetoGavanelliGlobalConstraint'):
-                solutions_in_time = get_solutions_in_time_for_choco(current_json_mo_solution_details["solver_messages"],
-                                                                    'all_solutions' in current_json_mo_solution_details,
-                                                                    filtered_data['front_generator'] == 'ParetoGavanelliGlobalConstraint')
-                front_metrics["solutions_in_time"] = solutions_in_time
-            current_json_mo_solution_details.pop("solver_messages")
-        filtered_data.update({field: current_json_mo_solution_details.get(field, None) for field
-                              in solution_details_fields})
-        filtered_data.update(front_metrics)
-        all_fields = list(filtered_data.keys())
-
-        double_check_gavanelli_front_strategy_stats_are_correc_for_choco(filtered_data, output["solutions-details"]["solver_messages"])
-
-        output_file_exists = os.path.isfile(sol_stats_filename)
-        existing_data = []
-        if output_file_exists:
-            with open(sol_stats_filename, 'r', newline='') as csvfile:
-                reader = csv.DictReader(csvfile)
-                existing_data = [row for row in reader]
-                existing_headers = reader.fieldnames
-                headers = existing_headers + [field for field in all_fields if field not in existing_headers]
-        else:
-            headers = all_fields
-
-        with open(sol_stats_filename, 'w', newline='') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=headers)
-            writer.writeheader()
-
-            if existing_data:
-                for row in existing_data:
-                    writer.writerow(row)
-            writer.writerow(filtered_data)
-        print(f"Processed {sys.argv[2]}")
-    else:
-        print(f"Error in {sys.argv[2]}, no processing", file=sys.stderr)
+    process_json_file(input_file_path, sol_stats_filename)
