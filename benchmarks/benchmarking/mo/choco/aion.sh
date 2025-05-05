5a72591cbcf4683c9aae1ebf10e343cd21ebaca8
Manuel Combarro Simon <manuel.combarrosimon@uni.lu>
2025-05-06 01:24:39 +0200

    Changes to aoin to effectively load Python

--- benchmarks/benchmarking/mo/choco/aion.sh
+++ benchmarks/benchmarking/mo/choco/aion.sh
@@ -3,5 +3,6 @@
 # See installation instructions for Choco.
 module use $HOME/.local/easybuild/${ULHPC_CLUSTER}/turbo/${RESIF_ARCH}/modules/all
 
+module load env/legacy/2020b
 module load lang/Python/3.8.6-GCCcore-10.2.0
 module load lang/Java/21.0.2 # for Choco
